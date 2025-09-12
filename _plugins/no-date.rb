ad9a8f81857d8738f5649893c40d3e64c189bc22
Exp <17885980+exp111@users.noreply.github.com>
2025-09-12 12:55:17 +0200

    no date

---
+++ _plugins/no-date.rb
@@ -0,0 +1,9 @@
+# reads posts without the required date
+class Jekyll::PostReader
+  def read_posts(dir)
+    read_publishable(dir, "_posts", /.*\.(markdown|md)$/)
+  end
+  def read_drafts(dir)
+    read_publishable(dir, "_drafts", /.*\.(markdown|md)$/)
+  end
+end
